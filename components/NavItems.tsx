805ff73256f5162f46ea3ef13b278bb31da73e4d
Abiel asimiea <dbestabi28@gmail.com>
2024-11-27 12:08:20 +0100

    building navbar

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,13 +1,18 @@
 "use client";
 import { PRODUCT_CATEGORIES } from "@/config";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/useOnClickOutside";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
   return (
-    <div className="flex gap-4 h-full ">
+    <div className="flex gap-4 h-full " ref={navRef}>
       {PRODUCT_CATEGORIES.map((product, index) => {
         const handleopen = () => {
           if (activeIndex === index) {
